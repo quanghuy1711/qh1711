54bfefc8003851e24e5ca69fd68902c722e603c0
quanghuy1711 <161600642+quanghuy1711@users.noreply.github.com>
2024-03-22 20:49:03 +0700

    Add files via upload

--- my.js
+++ my.js
@@ -1,24 +1,11 @@
-function cong(){
-    let a = +document.getElementById("a").value;
-    let b = +document.getElementById("b").value;
-    let x = a+b;
-    document.getElementById("result").innerHTML="KQ="+x;
+let content = "";
+content += "<table border>";
+for (let j= 1; j < 10; j++) {
+    content += "<tr>"
+    for (let i = 2; i < 10; i++) {
+        content += `<td>${i} x ${j} = ${i * j}</td>`;
+    }
+    content += "<td>"
 }
-function tru(){
-    let a = +document.getElementById("a").value;
-    let b = +document.getElementById("b").value;
-    let x = a-b;
-    document.getElementById("result").innerHTML="KQ="+x;
-}
-function nhan(){
-    let a = +document.getElementById("a").value;
-    let b = +document.getElementById("b").value;
-    let x = a*b;
-    document.getElementById("result").innerHTML="KQ="+x;
-}
-function chia(){
-    let a = +document.getElementById("a").value;
-    let b = +document.getElementById("b").value;
-    let x = a/b;
-    document.getElementById("result").innerHTML="KQ="+x;
-}
+content += "</table>";
+document.getElementById("result").innerHTML = content;
